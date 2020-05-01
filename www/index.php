f212b980dfeffc8c264f1f132247100f7cb5185a
Michael Rudd <audiostratus@yahoo.com>
2020-05-01 09:53:24 -0500

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Project Noah</title>
+	<script src='js/jquery-3.5.0.min.js'></script>
+	<script src='js/main.js'></script>
+	<link rel='stylesheet' href='css/main.css'>
+<body>
+	<div id='container'>
+		<ul id='brief-menu'>
+		</ul><!--end brief-menu-->
+		<ul id='main-menu'>
+		</ul><!--end main-menu-->
+		<div id='view-ports'>
+		</div><!--end view-ports-->
+	</div><!--end container-->
+</body>
+</html>
