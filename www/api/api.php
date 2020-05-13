8e713d170c43e37b4d88e2d7156a4566ecd40f16
Michael Rudd <audiostratus@yahoo.com>
2020-05-13 10:53:58 -0500

    Update api.php

--- www/api/api.php
+++ www/api/api.php
@@ -1,5 +1,24 @@
 <?php
 class NoahAPI{
+  public $requester_id = null;
+  public $authorized = 0;
+  public $scope = null;
+  
+  public function __construct($requester_id,$authorized,$scope){
+    $this->$requester_id = $requester_id;
+    $this->$authorized = $authorized;
+    $this->$scope = $scope;
+  }
+}
 
+class Users extends NoahAPI{
+  public $id = null;
+  public $date_added = null;
+  
+  public static $definition = array(
+    'table' => 'users',
+    'primary_key' => 'id',
+    'fields' => array('first_name','last_name','birthday','email','phones','street address','city','state','zipcode','relationship_to_owner','permissions','date_added')
+    );
 }
 ?>
