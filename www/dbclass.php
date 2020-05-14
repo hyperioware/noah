00d94064a5e3a4bbe0943c1bffa937a4ea29c0fa
Michael Rudd <audiostratus@yahoo.com>
2020-05-14 14:55:14 -0500

    Create dbclass.php

---
+++ www/dbclass.php
@@ -0,0 +1,25 @@
+<?php
+class DBClass{
+  private $host = "localhost";
+  private $username = "mrudd";
+  private $password = "Pass1981!";
+  private $database = "noah";
+
+  public $connection;
+
+  // get the database connection
+  public function getConnection(){
+
+      $this->connection = null;
+
+      try{
+          $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database, $this->username, $this->password);
+          $this->connection->exec("set names utf8");
+      }catch(PDOException $exception){
+          echo "Error: " . $exception->getMessage();
+      }
+
+      return $this->connection;
+  }
+}
+?>
