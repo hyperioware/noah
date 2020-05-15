e45f561e6bdceac6dee3fd690022835632eddb70
Michael Rudd <audiostratus@yahoo.com>
2020-05-15 14:42:41 -0500

    Update body.php

--- www/includes/body.php
+++ www/includes/body.php
@@ -1,1 +1,13 @@
-
+<?php
+echo "<body>
+	<div id='container'>
+		<ul id='brief-menu'>
+		</ul><!--end brief-menu-->
+		<ul id='main-menu'>
+		</ul><!--end main-menu-->
+		<div id='view-ports'>
+		</div><!--end view-ports-->
+	</div><!--end container-->
+</body>";
+  
+?>
