f4145d002f35fc6a56d67c334330e572824049b9
Michael Rudd <audiostratus@yahoo.com>
2020-05-15 14:41:26 -0500

    Update header.php

--- www/includes/header.php
+++ www/includes/header.php
@@ -1,1 +1,9 @@
+<?php
+echo "<head>
+	<title>Project Noah</title>
+	<script src='js/jquery-3.5.0.min.js'></script>
+	<script src='js/main.js'></script>
+	<link rel='stylesheet' href='css/main.css'>
+</head>";
 
+?>
