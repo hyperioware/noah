1e11fa24759298e661dc862af3b30c5ec0bbd146
Michael Rudd <audiostratus@yahoo.com>
2020-05-15 14:35:38 -0500

    Update index.php

--- www/api/index.php
+++ www/api/index.php
@@ -1,3 +1,40 @@
 <?php
+include "api.php";
+include "../../config/DBClass.php";
 
+session_start();
+/*
+OAuth Authentication TODO
+*/
+
+//Is user authenticated? Yes, then proceed to verify app authorization
+  //Is app authorized? Yes, then proceed to make request to API
+  //set $_SESSION['app_scope'] to assigned scope for app
+  
+  //No, then prompt user to validate authorization
+
+    //Does user validate authorization? Yes, then continue to API call
+    $scope; //set to assigned scope for app
+    //No, then send user to app callback URI
+
+//No, prompt user to authenticate and return to beginning
+  //Once authenticated, set user id to $_SESSION
+  
+function callAPI($scope){
+  $db = new DBClass();
+  $method = $_SERVER['REQUEST_METHOD'];
+  $currentUser = $_SESSION['current_user'];
+  $scope = $_SESSION['app_scope'];
+  switch($method){
+    case 'GET':
+      
+      break;
+    case 'POST':
+      break;
+    case 'PUT':
+      break;
+    case 'DELETE':
+      break;
+  }
+}
 ?>
